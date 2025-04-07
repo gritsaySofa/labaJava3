f9f4cda0b3ee21ee18b95ec649081e94d7da6b97
gritsaySofa <gritsay.sofiya@list.ru>
2025-04-07 14:46:04 +0300

    implementation of basic collection methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,62 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int n = 10000;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> linkedList = new LinkedList<>();
+
+        long start = System.nanoTime();
+        for (int i = 0; i < n; ++i) {
+            arrayList.add(i);
         }
+        long finish = System.nanoTime();
+        long arrayListAddTime = finish - start;
+
+        start = System.nanoTime();
+        for (int i = 0; i < n; ++i) {
+            linkedList.add(i);
+        }
+        finish = System.nanoTime();
+        long linkedListAddTime = finish - start;
+
+        // Testing get
+        start = System.nanoTime();
+        for (int i = 0; i < n; ++i) {
+            arrayList.get(i);
+        }
+        finish = System.nanoTime();
+        long arrayListGetTime = finish - start;
+
+        start = System.nanoTime();
+        for (int i = 0; i < n; ++i) {
+            linkedList.get(i);
+        }
+        finish = System.nanoTime();
+        long linkedListGetTime = finish - start;
+
+        start = System.nanoTime();
+        for (int i = n - 1; i >= 0; --i) {
+            arrayList.remove(i);
+        }
+        finish = System.nanoTime();
+        long arrayListRemoveTime = finish - start;
+
+        start = System.nanoTime();
+        for (int i = n - 1; i >= 0; --i) {
+            linkedList.remove(i);
+        }
+        finish = System.nanoTime();
+        long linkedListRemoveTime = finish - start;
+
+        System.out.println("Метод(" + n + " раз)\tArrayList\tLinkedList");
+        System.out.println("add\t\t\t\t\t" + arrayListAddTime + "\t\t" + linkedListAddTime);
+        System.out.println("get\t\t\t\t\t" + arrayListGetTime + "\t\t" + linkedListGetTime);
+        System.out.println("remove\t\t\t\t" + arrayListRemoveTime + "\t\t" + linkedListRemoveTime);
     }
 }
