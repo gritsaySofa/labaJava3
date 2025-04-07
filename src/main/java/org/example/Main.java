f7ba64d3fd34945c293c27532047a64b4e23169a
gritsaySofa <gritsay.sofiya@list.ru>
2025-04-07 14:56:35 +0300

    add JavaDoc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,21 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Класс для сравнения производительности операций ArrayList и LinkedList.
+ */
 public class Main {
+    /**
+     * Основной метод для тестирования производительности.
+     * @param args Аргументы командной строки
+     * */
     public static void main(String[] args) {
         int n = 10000;
 
         List<Integer> arrayList = new ArrayList<>();
         List<Integer> linkedList = new LinkedList<>();
 
+        // Тестирование операции добавления
         long start = System.nanoTime();
         for (int i = 0; i < n; ++i) {
             arrayList.add(i);
@@ -25,7 +33,7 @@ public class Main {
         finish = System.nanoTime();
         long linkedListAddTime = finish - start;
 
-        // Testing get
+        // Тестирование операции получения
         start = System.nanoTime();
         for (int i = 0; i < n; ++i) {
             arrayList.get(i);
@@ -40,6 +48,7 @@ public class Main {
         finish = System.nanoTime();
         long linkedListGetTime = finish - start;
 
+        // Тестирование операции удаления
         start = System.nanoTime();
         for (int i = n - 1; i >= 0; --i) {
             arrayList.remove(i);
@@ -54,6 +63,7 @@ public class Main {
         finish = System.nanoTime();
         long linkedListRemoveTime = finish - start;
 
+        // Вывод
         System.out.println("Метод(" + n + " раз)\tArrayList\tLinkedList");
         System.out.println("add\t\t\t\t\t" + arrayListAddTime + "\t\t" + linkedListAddTime);
         System.out.println("get\t\t\t\t\t" + arrayListGetTime + "\t\t" + linkedListGetTime);
